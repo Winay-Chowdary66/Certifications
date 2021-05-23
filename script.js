f5241f52dc3e80197f786d85a80b2b2aed870ce1
Annangi Vinay Chowdary <64681172+Winay-Chowdary66@users.noreply.github.com>
2021-05-23 17:37:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+preLoader = document.querySelector('.pre-loader')
 start = document.querySelector('.start');
 applications = document.querySelector('.applications');
 places = document.querySelector('.places');
@@ -9,6 +10,13 @@ trash = document.querySelector('.icon-container .trash');
 about = document.querySelector('.icon-container .about');
 currDateTime = document.querySelector('.cur-dat-tim');
 
+function preLoad(){
+	window.setInterval(() => {
+		preLoader.style.display = "none"
+	}, 1700);
+	
+}
+
 let curdate = new Date().toUTCString();
 curdate = curdate.split(' ').slice(0, 5).join(' ');
 currDateTime.innerText = curdate;
@@ -19,7 +27,8 @@ home.addEventListener('click', () => {
 		x: 'center',
 		y: 'center',
 		width: '50%',
-		height: '50%'
+		height: '50%',
+		url: "https://www.winay.lovestoblog.com/"
 	});
 });
 about.addEventListener('click', () => {
@@ -28,14 +37,13 @@ about.addEventListener('click', () => {
 		right: 50,
 		bottom: 0,
 		left: 50,
-		url: "https://github.com/Winay-Chowdary66"
+		url: "https://www.balaganesh.ga/gallery"
 	});
 });
 
 pictures.addEventListener('click', () => {
 	new WinBox({
 		title:'Pellichoopulu',
-		url : "https://winay-chowdary66.github.io/Certifications/MathOnrampCertificate.pdf"
-		
-	})
-})
+		url : "https://Winay-Chowdary66.github.io/python-program-convert-numbers-to-words"
+	});
+});
